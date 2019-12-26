e7f26991be5566eb6912d66c495e49cf9ef2ce51
johntsui <446192161@qq.com>
2019-12-26 21:36:15 +0800

    add config

---
+++ config-client/src/main/java/com/john/sc/configclient/controller/GitController.java
@@ -0,0 +1,21 @@
+package com.john.sc.configclient.controller;
+
+import com.john.sc.configclient.domain.GitConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+
+    @Autowired
+    private GitConfig gitConfig;
+
+
+
+    @GetMapping(value = "show")
+    public Object show(){
+        return gitConfig;
+    }
+
+}
