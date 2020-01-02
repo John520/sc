40c28c2f97c529fea02c084dfc81e0a1703674da
johntsui <446192161@qq.com>
2020-01-02 19:36:06 +0800

    add refresh scope

--- config-client/src/main/java/com/john/sc/configclient/domain/GitConfig.java
+++ config-client/src/main/java/com/john/sc/configclient/domain/GitConfig.java
@@ -3,10 +3,12 @@ package com.john.sc.configclient.domain;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
+@RefreshScope
 public class GitConfig {
 
     @Value("${data.env}")
